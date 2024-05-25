0b86c4f50d1bab0f0e707b59a528ec3fe29ccd44
LucaVaccaro <luca.vaccaro72@gmail.com>
2024-05-25 17:22:26 -0400

    added Address class with isPostalCodeValid method.

---
+++ src/main/java/org/example/Address.java
@@ -0,0 +1,105 @@
+package org.example;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private String province;
+    private String postalCode;
+    private String country;
+
+    public Address(int streetNo, String street, String city, String province, String postalCode, String country) {
+        if (isPostalCodeValid(postalCode)) {
+            this.streetNo = streetNo;
+            this.street = street;
+            this.city = city;
+            this.province = province;
+            this.postalCode = postalCode.toUpperCase();
+            this.country = country;
+        } else {
+            this.streetNo = 0;
+            this.street = null;
+            this.city = null;
+            this.province = null;
+            this.postalCode = null;
+            this.country = null;
+        }
+    }
+
+    /**
+     * checks if a postcode is valid or not. The length of a postal code can either be 6 or 7,
+     * if the length is 6, then it must follow the format: CDCDCD,
+     * if the length is 7, then it must follow the format: CDC DCD.
+     * where C is a character, while D is a digit. Case-insensitive;
+     * @param postalCode
+     * @return true  is the postal code is valid and false if it isn't
+     */
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null) {
+            return false;
+        }
+
+        postalCode = postalCode.toUpperCase();
+        int length = postalCode.length();
+
+        if (length != 6 && length != 7) {
+            return false;
+        }
+
+        if (length == 6) {
+
+            for (int i = 0; i < length; i++) {
+                char c = postalCode.charAt(i);
+                if (i % 2 == 0) {
+                    if (!(c >= 'A' && c <= 'Z')) {
+                        return false;
+                    }
+                } else {
+                    if (!(c >= '0' && c <= '9')) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        } else {
+            if (postalCode.charAt(3) != ' ') {
+                return false;
+            }
+            for (int i = 0; i < length; i++) {
+                char c = postalCode.charAt(i);
+                if (i == 3) continue;
+                if (i < 3) {
+                    if (i % 2 == 0) {
+                        if (!(c >= 'A' && c <= 'Z')) {
+                            return false;
+                        }
+                    } else {
+                        if (!(c >= '0' && c <= '9')) {
+                            return false;
+                        }
+                    }
+                } else {
+                    if ((i - 1) % 2 == 0) {
+                        if (!(c >= 'A' && c <= 'Z')) {
+                            return false;
+                        }
+                    } else {
+                        if (!(c >= '0' && c <= '9')) {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Address address = new Address(7050, "Mani Street", "Montreal", "Quebec", "H9D 0F5", "Quebec");
+
+        System.out.println(address);
     }
 }
